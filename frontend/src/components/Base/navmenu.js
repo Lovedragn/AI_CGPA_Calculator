754a4a22324c7c6bfd6e7ab0486e9c17b1ed3cd9
Sujith <sujith.sappani@gmail.com>
2025-04-27 14:18:55 +0530

    changes in navmenu layout

--- frontend/src/components/Base/navmenu.js
+++ frontend/src/components/Base/navmenu.js
@@ -88,7 +88,7 @@ export function Navmenu() {
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
-        <NavigationMenuItem>
+        <NavigationMenuItem >
           <NavigationMenuTrigger>Components</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
