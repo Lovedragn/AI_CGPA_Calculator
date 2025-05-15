e6d7737699883187e8c023593fbc3397e10b549d
Sujith <sujith.sappani@gmail.com>
2025-05-15 21:38:08 +0530

    Change in lofo cheome -512px

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,7 +3,7 @@ const GOOGLE_VERIFY = process.env.GOOGLE_VERIFY_ID;
 
 export const metadata = {
   title: "GPA Calculator",
-  description: "AI based CGPA calculator for College Students ",
+  description: "AI-powered CGPA calculator for precise grade evaluation and result tracking. Quickly compute semester-wise and cumulative GPAs with accuracy"
 };
 
 export default function RootLayout({ children }) {
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="manifest" href="/site.webmanifest"/>
-        {/* <link rel="icon" href="/logo.svg"/> */}
+        <link rel="icon" href="/logo.svg"/>
         <meta name="google-site-verification" content={GOOGLE_VERIFY} />
         <meta name="description" content="Free AI GPA calculator for students. Upload mark sheets and get instant CGPA results." />
         <meta name="keywords" content="GPA Calculator, CGPA Tool, Student Calculator, Marksheet Analyzer, Grade Calculator" />
