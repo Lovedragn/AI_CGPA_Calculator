dd2f1e300fe72ed81a69b1db63a5c92cba6e042a
Sujith <sujith.sappani@gmail.com>
2025-05-16 22:57:23 +0530

    Added title to AI GPA Calcy

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 const GOOGLE_VERIFY = process.env.GOOGLE_VERIFY_ID;
 
 export const metadata = {
-  title: "GPA Calculator",
+  title: "AI GPA Calcy",
   description: "AI-powered CGPA calculator for precise grade evaluation | Quickly compute semester-wise and cumulative GPAs accuracy"
 };
 
@@ -10,12 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>AI GPA Calcy</title>
         <link rel="manifest" href="/site.webmanifest"/>
         <link rel="icon" href="/logo.svg"/>
         <meta name="google-site-verification" content={GOOGLE_VERIFY} />
         <meta name="description" content="Free AI GPA calculator for students. Upload mark sheets and get instant CGPA results." />
-        <meta name="keywords" content="GPA Calculator, CGPA Tool, Student Calculator, Marksheet Analyzer, Grade Calculator" />
+        <meta name="keywords" content="GPA Calculator, CGPA Tool, Student Calculator, Marksheet Analyzer, Grade Calculator,GPA,GPA Calcy , CGPA , CGPA calcy , cgpa , gpa calculator , cgpa calculator,anna university calculator,grtiet calculator" />
         <meta name="robots" content="index, follow" />
         <meta property="og:title" content="GPA Calculator" />
         <meta property="og:description" content="Calculate GPA automatically by uploading your mark sheet." />
