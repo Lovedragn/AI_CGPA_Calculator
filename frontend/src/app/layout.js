a50c404aa0d3b84554df191866e4537671873bf8
Sujith <sujith.sappani@gmail.com>
2025-05-16 22:54:51 +0530

    Added title to AI GPA Calcy

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,20 +3,21 @@ const GOOGLE_VERIFY = process.env.GOOGLE_VERIFY_ID;
 
 export const metadata = {
   title: "GPA Calculator",
-  description: "AI-powered CGPA calculator for precise grade evaluation and result tracking. Quickly compute semester-wise and cumulative GPAs with accuracy"
+  description: "AI-powered CGPA calculator for precise grade evaluation | Quickly compute semester-wise and cumulative GPAs accuracy"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>AI GPA Calcy</title>
         <link rel="manifest" href="/site.webmanifest"/>
         <link rel="icon" href="/logo.svg"/>
         <meta name="google-site-verification" content={GOOGLE_VERIFY} />
         <meta name="description" content="Free AI GPA calculator for students. Upload mark sheets and get instant CGPA results." />
         <meta name="keywords" content="GPA Calculator, CGPA Tool, Student Calculator, Marksheet Analyzer, Grade Calculator" />
         <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="CGPA Calculator" />
+        <meta property="og:title" content="GPA Calculator" />
         <meta property="og:description" content="Calculate GPA automatically by uploading your mark sheet." />
         <meta property="og:url" content="https://ai-cgpa-calculator.vercel.app/" />
         <meta property="og:type" content="website" />
